ecfecfefcec48d2278d9711b079ce29b53f962f3
waylon.yuen <waylon.yuen@gosmio.biz>
2023-06-10 13:25:09 +0800

    Migration

---
+++ MeowkitPy/MeowkitPy/logging/logger.py
@@ -0,0 +1,24 @@
+﻿# Flask 下有 app.logger.info()
+
+class Color:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+class Logger():
+
+    def LogInfomation(info: str):
+        print(f'   [Debug]: {info}')
+
+    def LogWarning(info: str):
+        print(f' ! [Warning]: {info}')
+
+    def LogError(info: str):
+        print(f' @ [Error]: {info}')
+
+# Global
+log = Logger
---
+++ MeowkitPy/program.py
@@ -0,0 +1,5 @@
+
+from MeowkitPy import app
+
+if __name__ == '__main__':
+    pass
